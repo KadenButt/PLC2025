1f0b0430a9f1e22a3ce0f14f7ed842750b3af710
Kaden Butt <kadenjbutt@gmail.com>
2025-01-28 14:22:23 +0000

    lab 2 done

--- lab2-functionalprimer/javascript/func_as_arg.js
+++ lab2-functionalprimer/javascript/func_as_arg.js
@@ -1,24 +1,24 @@
 //Create list of ints from 1 to 5, Haskell equivalent [1..5]
-function arrFunc(){
+function arrFunc(a ,b ){
     let arr = [];
-    for (let i = 1; i<=5; i++) {
+    for (let i = a; i<=b; i++) {
         arr.push(i);
     }
     return arr;    
 }
 
-function applicatorFunc(inpFunc, s){
+function applicatorFunc(a, b, s){
     if(s=='s'){
-        const arr = inpFunc();        
+        const arr = arrFunc(a, b);        
         let sum = arr.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
         return sum;
     }
     else{        
-        const arr = inpFunc();
+        const arr = arrFunc(a, b);
         let sum = arr.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
         return sum/5;
     }
 }
 
-let x = applicatorFunc(arrFunc, 's');
+let x = applicatorFunc(1, 100, 's');
 console.log(x);
